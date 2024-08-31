f03d2dd12d3eb685e68a512658b5597683fda6c1
Zxy <zhangxuanyang698@gmail.com>
2024-08-31 19:43:51 +0800

    Initial Commit

---
+++ RedBlackTree/RedBlackTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedBlackTreeApp.swift
+//  RedBlackTree
+//
+//  Created by 张炫阳 on 2024/8/31.
+//
+
+import SwiftUI
+
+@main
+struct RedBlackTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
